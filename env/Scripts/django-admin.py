32879d7e7d502078d6b3556fd6ced6d50bd458dc
Tehn1 <eihseo@hotmail.com>
2020-05-23 16:14:06 +0100

    will it work

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jpb69\documents\new laptop\experiments\m experiment plans - updated virtual screen\experiment m21\pug3\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
